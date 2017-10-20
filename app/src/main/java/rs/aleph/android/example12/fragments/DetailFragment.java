64d115f4eed9d48f759fd7a7cc4889e890803e4d
DanijelaKaraklic <danijela.popovic987@gmail.com>
2017-10-20 20:30:26 +0200

    Dodati resursi za fragmente

---
+++ app/src/main/java/rs/aleph/android/example12/fragments/DetailFragment.java
@@ -0,0 +1,10 @@
+package rs.aleph.android.example12.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by androiddevelopment on 20.10.17..
+ */
+
+public class DetailFragment extends Fragment {
+}
